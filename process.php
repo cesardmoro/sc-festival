e77729c8bbc7e90d67112082d0057b7601af41cb
Cesar Moro <cesar.d.moro@gmail.com>
2023-06-08 10:58:01 -0300

    added email

---
+++ process.php
@@ -0,0 +1,30 @@
+axn: contact_2
+username: test
+email: cesarasd
+subject: asdasd
+description: asd
+
+<?php 
+$to = "cesar.d.moro@gmail.com";
+$subject = "Somos Cerveceros | Contacto Web Festival ";
+
+$message = "<b>Este es un contacto desde la web del festival </b>";
+$message .= "<h1>This is headline.</h1>";
+
+$message .= "<br>Nombre:".$_POST['username'];
+$message .= "<br>Email:".$_POST['email'];
+$message .= "<br>Asunto:".$_POST['subject'];
+$message .= "<br>Descripcion:".$_POST['Descripcion'];
+
+$header = "From:info@somoscerveceros.com \r\n";
+$header .= "MIME-Version: 1.0\r\n";
+$header .= "Content-type: text/html\r\n";
+
+$retval = mail ($to,$subject,$message,$header);
+
+if( $retval == true ) {
+   echo "Message sent successfully...";
+}else {
+   echo "Message could not be sent...";
+}
+?>
